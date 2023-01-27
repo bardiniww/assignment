470be98ebf02468a1d242021a7a551defcd3f4f9
bardiniww <bardiniww@gmail.com>
2023-01-27 22:15:44 +0800

    cash migrations added

--- cash/src/main/resources/db/migration/V01.00.00__init_tables.sql
+++ cash/src/main/resources/db/migration/V01.00.00__init_tables.sql
@@ -1,5 +1,29 @@
-CREATE TABLE IF NOT EXISTS cash_agent (
-    id BIGSERIAL PRIMARY KEY,
-    account_id INT UNIQUE NOT NULL,
-    type_id INT NOT NULL
+CREATE TABLE cash_agent_type (
+    id INT NOT NULL UNIQUE,
+    description VARCHAR(500) NOT NULL
 );
+
+INSERT INTO cash_agent_type VALUES
+(1, 'Pure customer'),
+(2, 'Huge company');
+
+CREATE TABLE cash_agent (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    account_id BIGINT UNIQUE NOT NULL,
+    type_id INT NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES cash_agent_type (id)
+);
+
+CREATE TABLE cash_transaction (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    creditor_cash_agent_id BIGINT NOT NULL,
+    debtor_cash_agent_id BIGINT NOT NULL,
+    amount DECIMAL NOT NULL,
+    FOREIGN KEY (creditor_cash_agent_id) REFERENCES cash_agent (id),
+    FOREIGN KEY (debtor_cash_agent_id) REFERENCES cash_agent (id)
+);
+
+CREATE INDEX cash_transaction_creditor_cash_agent_id_idx on cash_transaction using btree (creditor_cash_agent_id);
+CREATE INDEX cash_transaction_debtor_cash_agent_id_idx on cash_transaction using btree (debtor_cash_agent_id);
+
+
