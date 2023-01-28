78ebc72d534c6a233a3334ada8a573f0b531e9f5
bardiniww <bardiniww@gmail.com>
2023-01-28 19:02:55 +0800

    eureka clients registered by services

--- cash/src/main/java/com/bardiniww/cash/CashApplication.java
+++ cash/src/main/java/com/bardiniww/cash/CashApplication.java
@@ -2,8 +2,10 @@ package com.bardiniww.cash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CashApplication {
     public static void main(String[] args) {
         SpringApplication.run(CashApplication.class, args);
